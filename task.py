345fabbc3bee55a45ffb8da3d22feb6949f24f32
MozzhukhinRA <roma.moz@mail.ru>
2025-07-28 16:13:46 +0300

    New Home Work

--- task.py
+++ task.py
@@ -96,15 +96,18 @@ def test_readable_function():
 
 def open_browser(browser_name):
     actual_result = convert_function_and_all_arguments(open_browser, browser_name)
+    print('\n', actual_result)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
     actual_result = convert_function_and_all_arguments(go_to_companyname_homepage, page_url)
+    print(actual_result)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
     actual_result = convert_function_and_all_arguments(find_registration_button_on_login_page, page_url, button_text)
+    print(actual_result)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
 
